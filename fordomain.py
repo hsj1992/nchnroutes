b9d4e4a05b2bf59581f87e08ee537b7029f8fba5
root <root@PaoPao-Pref.192.168.31.1>
2023-12-31 14:26:39 +0800

    bug fix

--- fordomain.py
+++ fordomain.py
@@ -6,10 +6,11 @@ import requests
 def nslookup_domain(domain):
     try:
         # 使用 socket.getaddrinfo 获取域名对应的 IP 地址
-        _, _, _, _, (ip_address, _) = socket.getaddrinfo(domain, None, socket.AF_INET)[0]
-        return ip_address
+        addr_info = socket.getaddrinfo(domain, None, socket.AF_INET)
+        ip_addresses = [ip for _, _, _, _, (ip, _) in addr_info]
+        return ip_addresses
     except (socket.gaierror, IndexError, UnicodeError):
-        return None
+        return []
 
 def process_url(url):
     ip_array = []
@@ -20,7 +21,8 @@ def process_url(url):
         response.raise_for_status()  # Raise an HTTPError for bad responses (e.g., 404)
 
         for line in response.text.split('\n'):
-            domain = line.strip().replace('domain:', '')
+            # 使用 split 方法以冒号为分隔符，获取冒号后的域名部分
+            domain = line.strip().split(':', 1)[-1].strip()
             ip = nslookup_domain(domain)
             if ip:
                 ip_array.append(ip)
@@ -42,9 +44,14 @@ def process_url(url):
 # 示例用法
 force_cn_list_url = 'http://192.168.31.4:7889/force_cn_list.txt'
 ip_array_force_cn = process_url(force_cn_list_url)
+# 替换最后一位为0并添加子网掩码长度
+modified_ips = [f"{ip.rsplit('.', 1)[0]}.0/24" for sublist in ip_array_force_cn for ip in sublist if ip != '0.0.0.1']
+
+# 去重
+flat_ip_array = list(set(modified_ips))
 
 # 将 ip_array_force_cn 用于 produce.py 的 --exclude 参数
-exclude_param = " ".join(ip for ip in ip_array_force_cn)
+exclude_param = " ".join(ip for ip in flat_ip_array)
 print(f"\nUsing --exclude parameter in produce.py: {exclude_param}")
 
 # 执行 produce.py 脚本，传递 --next 参数
